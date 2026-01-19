c9e67395bd22b528ead2a06adeedce66b85606e9
woseayrch <3303341078@qq.com>
2026-01-19 09:25:22 +0800

    Update 32_node0.sh

--- scripts/wan2.2/32_node0.sh
+++ scripts/wan2.2/32_node0.sh
@@ -12,7 +12,7 @@ accelerate launch \
   --zero_stage 2 \
   --num_machines 4 \
   --machine_rank 0 \
-  --main_process_ip 100.64.72.187 \
+  --main_process_ip 100.64.152.190 \
   --main_process_port 29500 \
   --num_processes 32 \
   --max_restarts 10 \
