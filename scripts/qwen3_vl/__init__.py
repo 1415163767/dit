215c6ccafa12a38354d0c936bc13bb70fe4644b7
woseayrch <3303341078@qq.com>
2026-01-20 09:49:04 +0800

    Update __init__.py

--- scripts/qwen3_vl/__init__.py
+++ scripts/qwen3_vl/__init__.py
@@ -2,3 +2,4 @@ from .configuration_qwen3_vl import *
 from .modeling_qwen3_vl import *
 from .processing_qwen3_vl import *
 from .video_processing_qwen3_vl import *
+from .transformer_encoder import *
