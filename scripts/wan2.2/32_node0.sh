bd0350e4d4f080b3513fe5bc48fc52bd832b0486
woseayrch <3303341078@qq.com>
2026-02-14 11:24:24 +0800

    Update 32_node0.sh

--- scripts/wan2.2/32_node0.sh
+++ scripts/wan2.2/32_node0.sh
@@ -12,7 +12,7 @@ accelerate launch \
   --zero_stage 2 \
   --num_machines 4 \
   --machine_rank 0 \
-  --main_process_ip 100.64.171.96 \
+  --main_process_ip 100.66.52.170 \
   --main_process_port 29500 \
   --num_processes 32 \
   --deepspeed_config_file config/zero_stage2_config.json \
