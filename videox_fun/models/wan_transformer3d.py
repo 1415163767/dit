c6c5d1d4762857923b4b6e5a1450c95a0e61556e
woseayrch <3303341078@qq.com>
2026-04-14 10:53:52 +0800

    Refactor forward method for clarity and structure

--- videox_fun/models/wan_transformer3d.py
+++ videox_fun/models/wan_transformer3d.py
@@ -823,24 +823,24 @@ class ViTFeatureUpsampler(nn.Module):
         self.pre_proj = nn.Linear(in_dim, out_dim)
 
     def forward(self, vit_feat, T, H, W, target_H, target_W):
-        # B, L, C = vit_feat.shape
-        # # 1. 维度投影
-        # x = self.pre_proj(vit_feat) # [B, L, out_dim]
-        # # 2. 还原空间结构
-        # x = x.transpose(1, 2).view(B, self.out_dim, T, H, W)
-        # # 3. 逐帧上采样 (将 T 维度并入 Batch) 
-        # x_frames = x.permute(0, 2, 1, 3, 4).contiguous().view(B * T, self.out_dim, H, W)
-        # x_upsampled = F.interpolate(x_frames, size=(target_H, target_W), mode='bilinear', align_corners=False)
-        # # 4. 拉平回序列
-        # x_final = x_upsampled.view(B, T, self.out_dim, target_H, target_W)
-        # return x_final.permute(0, 2, 1, 3, 4).flatten(2).transpose(1, 2)
         B, L, C = vit_feat.shape
-        x = vit_feat.transpose(1, 2).view(B, C, T, H, W)
-        x_frames = x.permute(0, 2, 1, 3, 4).contiguous().view(B * T, C, H, W)
+        # 1. 维度投影
+        x = self.pre_proj(vit_feat) # [B, L, out_dim]
+        # 2. 还原空间结构
+        x = x.transpose(1, 2).view(B, self.out_dim, T, H, W)
+        # 3. 逐帧上采样 (将 T 维度并入 Batch) 
+        x_frames = x.permute(0, 2, 1, 3, 4).contiguous().view(B * T, self.out_dim, H, W)
         x_upsampled = F.interpolate(x_frames, size=(target_H, target_W), mode='bilinear', align_corners=False)
-        x_final = x_upsampled.view(B, T, C, target_H, target_W).permute(0, 2, 1, 3, 4).flatten(2).transpose(1, 2)
-        x = self.pre_proj(x_final) # [B, L, out_dim]
-        return x
+        # 4. 拉平回序列
+        x_final = x_upsampled.view(B, T, self.out_dim, target_H, target_W)
+        return x_final.permute(0, 2, 1, 3, 4).flatten(2).transpose(1, 2)
+        # B, L, C = vit_feat.shape
+        # x = vit_feat.transpose(1, 2).view(B, C, T, H, W)
+        # x_frames = x.permute(0, 2, 1, 3, 4).contiguous().view(B * T, C, H, W)
+        # x_upsampled = F.interpolate(x_frames, size=(target_H, target_W), mode='bilinear', align_corners=False)
+        # x_final = x_upsampled.view(B, T, C, target_H, target_W).permute(0, 2, 1, 3, 4).flatten(2).transpose(1, 2)
+        # x = self.pre_proj(x_final) # [B, L, out_dim]
+        # return x
 
 WAN_CROSSATTENTION_CLASSES = {
     't2v_cross_attn': WanT2VCrossAttention,
