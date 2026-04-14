56541783d59beaa44bc3b53ef366cb60f04f4275
woseayrch <3303341078@qq.com>
2026-04-14 11:47:35 +0800

    Update train_amlt.sh

--- scripts/wan2.2/train_amlt.sh
+++ scripts/wan2.2/train_amlt.sh
@@ -83,7 +83,7 @@ accelerate launch \
   --video_sample_stride=1 \
   --vit_sample_stride=2 \
   --video_sample_n_frames=121 \
-  --resolution_list "(512,288)" "(288,512)" \
+  --resolution_list "(288,512)" \
   --train_batch_size=1 \
   --resume_from_checkpoint "latest" \
   --gradient_accumulation_steps=1 \
